34d4258850cbc57073c004fab69bb087138a9c0f
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-12 19:31:27 +0530

    reversing linked list

---
+++ linked_list/206.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def reverseList(head):
+    prev = None
+    curr = head
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    return prev
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+
+result = reverseList(head)
+while result:
+    print(result.val, end=" → ")
+    result = result.next
+print("None")
