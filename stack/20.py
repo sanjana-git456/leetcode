dc7e5cd30d842e1cc7a2902f0b5ad4df7214f63e
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-19 23:07:04 +0530

    corrected

--- stack/20.py
+++ stack/20.py
@@ -1,13 +1,17 @@
 x = input("Enter: ")
 d = {')' : '(' , ']' : '[' , '}' : '{'}
 a = []
+valid = True
 for i in x:
     if i in d:
         if a and a[-1] == d[i]:
             a.pop()
         else:
-            print("Invalid")
+            valid = False
+            break
     else:
         a.append(i)
-if a == []:
+if valid and a == []:
     print("Valid")
+else:
+    print("Invalid")
