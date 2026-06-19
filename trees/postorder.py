34bfd9dcf860f7ff71a39e6de7f61b11ef2f69da
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-19 19:34:16 +0530

    postorder

---
+++ trees/postorder.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+def postorder(node):
+    if node is None:
+        return
+    postorder(node.left)
+    postorder(node.right)
+    print(node.val)
+postorder(root)
