15ad4459a26df46d86968bfa7362172002cdc13d
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-30 23:56:57 +0530

    coin problem

--- dp/322.py
+++ dp/322.py
@@ -1,14 +1,11 @@
 coins = list(map(int, input("Enter coins: ").split()))
 amount = int(input("Enter amount: "))
-
 dp = [float('inf')] * (amount + 1)
 dp[0] = 0
-
 for i in range(1, amount + 1):
     for coin in coins:
         if coin <= i:
             dp[i] = min(dp[i], dp[i - coin] + 1)
-
 if dp[amount] == float('inf'):
     print(-1)
 else:
