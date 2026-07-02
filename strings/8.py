cd41f83802643e2da49720cb3b94338fbcbe1d85
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-03 04:02:41 +0530

    simpler way

--- strings/8.py
+++ strings/8.py
@@ -1,9 +1,6 @@
 x = int(input("Enter: "))
 s = str(abs(x))
-l = []
-for i in s:
-    l.append(i)
 if x >= 0:
-    print(''.join(l[::-1]))
+    print(''.join(s[::-1]))
 else:
-    print("-"+''.join(l[::-1]))
+    print("-"+''.join(s[::-1]))
---
+++ strings/9.py
@@ -0,0 +1,6 @@
+x = int(input("Enter: "))
+x = str(x)
+print(x[::-1])
+# l = []
+# for i in x:
+#     l.append(i)
